d03eb41bca95c70a2fdc478576bb4ba72c15f442
franshelina camilo <franshelina_camilo@ucne.edu.do>
2026-09-12 23:07:45 -0400

    Agregar service y metodos Buscar, Eliminar y GetList

---
+++ RegistroLibros/Services/LibroService.cs
@@ -0,0 +1,34 @@
+﻿using Aplicada1.Core;
+using Microsoft.EntityFrameworkCore;
+using RegistroLibros.Context;
+using RegistroLibros.Models;
+using System.Linq.Expressions;
+
+namespace RegistroLibros.Services
+{
+    public class LibroService(IDbContextFactory<Contexto> contextFactory) : IService<Libro, int>
+    {
+        public async Task<Libro?> Buscar(int LibroId)
+        {
+            await using var contexto = await contextFactory.CreateDbContextAsync();
+            return await contexto.Libros
+                .Include(a => a.Autor)
+                .FirstOrDefaultAsync(l => l.LibroId == LibroId);
+        }
+
+        public Task<bool> Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Libro>> GetList(Expression<Func<Libro, bool>> criterio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Guardar(Libro entidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
