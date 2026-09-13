922e9bdf8b1ac82a7405e48088f353c6e791268a
franshelina camilo <franshelina_camilo@ucne.edu.do>
2026-09-12 21:26:44 -0400

    Crear proyecto y agregar modelo libro

---
+++ RegistroLibros/Models/Libro.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroLibros.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int LibroId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Titulo { get; set; } = null!;
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Autor { get; set; } = null!;
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public int AnioPublicacion { get; set; }
+
+    }
+}
