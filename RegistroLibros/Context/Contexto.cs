dfc9cf0beb6e3b8d15d94d2492cb13fe8b86fffd
franshelina camilo <franshelina_camilo@ucne.edu.do>
2026-09-12 22:31:02 -0400

    Crear base de datos

--- RegistroLibros/Context/Contexto.cs
+++ RegistroLibros/Context/Contexto.cs
@@ -1,6 +1,13 @@
-﻿namespace RegistroLibros.Context
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroLibros.Models;
+
+namespace RegistroLibros.Context
 {
-    public class Contexto
+    public class Contexto : DbContext  
     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+        public DbSet<Libro> Libros { get; set; } = null!;
     }
 }
