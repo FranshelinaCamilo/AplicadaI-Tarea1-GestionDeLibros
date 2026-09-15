96e153d476aaff03876ce808acafa53c3fc27852
franshelina camilo <franshelina_camilo@ucne.edu.do>
2026-09-14 21:26:18 -0400

    changes

---
+++ RegistroLibros/Models/Partidas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroLibros.Models
+{
+    public class Partidas
+    {
+        [Key]
+        public int PartidaId { get; set; }
+
+        [ForeignKey("Jugador")]
+        public int JugadorId { get; set; }
+
+        public int Puntuacion { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
--- RegistroLibros/Program.cs
+++ RegistroLibros/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
-builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
 
 builder.Services.AddScoped<LibroService>();
 
